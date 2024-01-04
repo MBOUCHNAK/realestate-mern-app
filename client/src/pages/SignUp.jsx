ec8eab15ed47070c9f37f48d4b50baff0f7a6583
Mejda <mejdabouchnak@gmail.com>
2024-01-04 21:08:42 +0100

    firebase config

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
+
 
 const SignUp = () => {
     
@@ -85,7 +87,9 @@ const SignUp = () => {
                     "
                 >
                     {loading ? "Loading..." : 'Sign Up'}
+                    
                 </button>
+                <OAuth/>
             </form>
             <div className='flex gap-2 mt-5'>
                 <p>Have an account ?</p>
