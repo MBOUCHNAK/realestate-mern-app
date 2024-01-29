07f0fbb9c6bf0f903e60f264448a2d57247e1b01
Mejda <mejdabouchnak@gmail.com>
2024-01-29 20:57:32 +0100

    minor change

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,8 +3,8 @@ import Listing from "../models/listing.model.js"
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
-        return res.statuts(201).json(listing)
+        return res.status(201).json(listing);
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
