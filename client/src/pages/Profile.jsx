ef31fc207c4b516b669b0cbcf65698525c54c64e
Mejda <mejdabouchnak@gmail.com>
2024-02-01 19:33:15 +0100

    update listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -165,7 +165,10 @@ setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
                 <h1 className="text-3xl font-semiblod text-center my-7">
                   Profile
                 </h1>
-                <form onSubmit={handleSubmit} className='flex flex-col gap-4 mt-5'>
+                <form
+                  onSubmit={handleSubmit}
+                  className="flex flex-col gap-4 mt-5"
+                >
                   <input
                     type="file"
                     ref={fileRef}
@@ -227,52 +230,91 @@ setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
                     disabled:opacity-80"
                     disabled={loading}
                   >
-                    {loading ? 'Loading...': 'Update'}
+                    {loading ? "Loading..." : "Update"}
                   </button>
-                  
-<Link
-to={"/create-listing"}
-className='
+
+                  <Link
+                    to={"/create-listing"}
+                    className="
 bg-green-700
 text-white
 p-3
 rounded-lg
 uppercase
 text-center
-hover:opacity-95'
->
-  Create Listing
-</Link>
+hover:opacity-95"
+                  >
+                    Create Listing
+                  </Link>
                 </form>
                 <div className="flex justify-between gap-4 mt-5">
-                  <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
+                  <span
+                    onClick={handleDeleteUser}
+                    className="text-red-700 cursor-pointer"
+                  >
                     Delete account
                   </span>
-                  <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
+                  <span
+                    onClick={handleSignOut}
+                    className="text-red-700 cursor-pointer"
+                  >
+                    Sign Out
+                  </span>
                 </div>
-                <p className='text-red-700 mt-5'>{error ? error : ''}</p>
-                <p className='text-green-700 mt-5'>{updateSuccess ? 'Profile successfully updated' : ''}</p>
-                <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
-                <p className='text-red-700 mt-5 text-center'>{showListingsError ? 'Error showing listings' : ''}</p>
+                <p className="text-red-700 mt-5">{error ? error : ""}</p>
+                <p className="text-green-700 mt-5">
+                  {updateSuccess ? "Profile successfully updated" : ""}
+                </p>
+                <button
+                  onClick={handleShowListings}
+                  className="text-green-700 w-full"
+                >
+                  Show Listings
+                </button>
+                <p className="text-red-700 mt-5 text-center">
+                  {showListingsError ? "Error showing listings" : ""}
+                </p>
 
-                {userListings && userListings.length > 0 &&
-                    <div className='flex flex-col gap-4'>
-                      <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
-                      {userListings.map((listing) => (
-                          <div key={listing._id}  className='rounded-lg border p-3 flex justify-between items-center gap-4'>
-                              <Link to={`/listing/${listing._id}`}>
-                                <img src={listing.imageUrls[0]} alt="listing cover" className='w-16 h-16 object-contain' />
-                    </Link>
-                    <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
-                      <p>{listing.name}</p>
-                    </Link>
-                    <div className='flex flex-col items-center'>
-                      <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                      <button className='text-green-700 uppercase'>Edit</button>
-                    </div>
+                {userListings && userListings.length > 0 && (
+                  <div className="flex flex-col gap-4">
+                    <h1 className="text-center mt-7 text-2xl font-semibold">
+                      Your Listings
+                    </h1>
+                    {userListings.map((listing) => (
+                      <div
+                        key={listing._id}
+                        className="rounded-lg border p-3 flex justify-between items-center gap-4"
+                      >
+                        <Link to={`/listing/${listing._id}`}>
+                          <img
+                            src={listing.imageUrls[0]}
+                            alt="listing cover"
+                            className="w-16 h-16 object-contain"
+                          />
+                        </Link>
+                        <Link
+                          className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                          to={`/listing/${listing._id}`}
+                        >
+                          <p>{listing.name}</p>
+                        </Link>
+                        <div className="flex flex-col items-center">
+                          <button
+                            onClick={() => handleListingDelete(listing._id)}
+                            className="text-red-700 uppercase"
+                          >
+                            Delete
+                          </button>
+                          <Link to={`/update-listing/${listing._id}`}>
+                            <button className="text-green-700 uppercase">
+                              Edit
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>}
+                )}
               </div>
             );
 };
